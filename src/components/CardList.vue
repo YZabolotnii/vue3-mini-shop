e928cde5fd37c01facb245d3a5fa8e3b24c3d3c0
Yaroslav <131529206+YZabolotnii@users.noreply.github.com>
2024-09-24 17:42:25 +0300

    Refactor cardList

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -4,21 +4,52 @@ import { useSneakersStore } from "@/stores/counter";
 
 const sneakersStore = useSneakersStore();
 
-sneakersStore.getSneakers().then(() => {
-  console.log(sneakersStore.sneakers);
-});
+const onChangeSelect = (event: any) => {
+  sneakersStore.filters.sortBy = event.target.value;
+  sneakersStore.getSneakers();
+}
+
+const onChangeSearchInput = (event: any) => {
+  sneakersStore.filters.searchQuery = event.target.value;
+  sneakersStore.getSneakers();
+}
 
 const onClickAdd = () => {
   console.log('clicked add');
 };
 
-// Функція для оновлення улюблених
 const onClickFavourite = (id: number, isFavorite: boolean) => {
-  sneakersStore.updateSneakers(id, !isFavorite); // Передаємо нове значення isFavorite
+  sneakersStore.updateSneakers(id, !isFavorite);
 };
+
+sneakersStore.getSneakers();
 </script>
 
 <template>
+
+  <div class="flex justify-between items-center mb-10">
+    <h1 class="text-3xl font-bold">Все кроссовки</h1>
+    <div class="flex items-center gap-4">
+      <select @change="onChangeSelect" class="py-2 px-3 border border-gray-200 focus:border-gray-400 rounded-md focus:outline-none">
+        <option value="title">По названию</option>
+        <option value="price">По цене (дешевые)</option>
+        <option value="-price">По цене (дорогие)</option>
+      </select>
+      <div class="relative">
+        <input
+            type="text"
+            @input="onChangeSearchInput"
+            class="border border-gray-200 rounded-md py-2 pl-10 pr-4 focus:outline-none focus:border-gray-400"
+            placeholder="Пошук..."
+        />
+        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <img alt="Search" src="/search.svg" />
+        </div>
+      </div>
+    </div>
+  </div>
+
+
   <div class="grid grid-cols-4 gap-10">
     <Card
         v-for="item in sneakersStore.sneakers"
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,13 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
 import CardList from '@/components/CardList.vue';
-import { useSneakersStore } from "@/stores/counter.ts";
-import {onMounted} from "vue";
-
-const sneakersStore = useSneakersStore();
-
-const onChangeSelect = (event: any) => {
-  sneakersStore.filters.sortBy = event.target.value;
-  sneakersStore.getSneakers();
-}
-
-const onChangeSearchInput = (event: any) => {
-  sneakersStore.filters.searchQuery = event.target.value;
-  sneakersStore.getSneakers();
-}
-
-sneakersStore.getSneakers();  // Початкове завантаження кросівок
-
 </script>
 
 <template>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl shadow-grey-200 mt-2">
     <Header />
-
     <div class="p-10">
-      <div class="flex justify-between items-center mb-10">
-        <h1 class="text-3xl font-bold">Все кроссовки</h1>
-        <div class="flex items-center gap-4">
-          <select @change="onChangeSelect" class="py-2 px-3 border border-gray-200 focus:border-gray-400 rounded-md focus:outline-none">
-            <option value="title">По названию</option>
-            <option value="price">По цене (дешевые)</option>
-            <option value="-price">По цене (дорогие)</option>
-          </select>
-          <div class="relative">
-            <input
-                type="text"
-                @input="onChangeSearchInput"
-                class="border border-gray-200 rounded-md py-2 pl-10 pr-4 focus:outline-none focus:border-gray-400"
-                placeholder="Пошук..."
-            />
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <img alt="Search" src="/search.svg" />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <CardList :items="sneakersStore.sneakers" />
+      <CardList/>
     </div>
   </div>
 </template>
